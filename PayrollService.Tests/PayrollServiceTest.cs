40a8116390e0642721209ccab34982c43a614742
Tobias.Berger <Tobias.Berger@begis.de>
2020-02-26 20:06:18 +0100

    first working test

---
+++ PayrollService.Tests/PayrollServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PayrollService.Controllers;
+
+namespace PayrollService.Tests
+{
+    [TestClass]
+    public class PayrollServiceTest
+    {
+        [TestMethod]
+        public void ThereShouldBeAPayrollServiceController()
+        {
+            // Arrange
+            var controller = new PayrollServiceController();
+
+            // Assert
+            Assert.IsNotNull(controller);
+        }
+    }
+}
