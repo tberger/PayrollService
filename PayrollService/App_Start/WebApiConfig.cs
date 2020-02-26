41845b33121e9e29a038b206bb342f58b0263908
Tobias.Berger <Tobias.Berger@begis.de>
2020-02-26 20:34:28 +0100

    removed the default route

--- PayrollService/App_Start/WebApiConfig.cs
+++ PayrollService/App_Start/WebApiConfig.cs
@@ -13,12 +13,6 @@ namespace PayrollService
 
             // Web-API-Routen
             config.MapHttpAttributeRoutes();
-
-            config.Routes.MapHttpRoute(
-                name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
-            );
         }
     }
 }
