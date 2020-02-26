2726da6513cc0da807f3381e2ee6adbc077ff009
Tobias.Berger <Tobias.Berger@begis.de>
2020-02-26 21:52:21 +0100

    fixed broken code

---
+++ PayrollService/Services/GermanTaxDeductionCalculator.cs
@@ -0,0 +1,14 @@
+﻿using PayrollService.Services.Interfaces;
+
+namespace PayrollService.Services
+{
+    public class GermanTaxDeductionCalculator : ICountryTaxCalculator
+    {
+        public decimal CalculateTaxesDeduction(decimal grossIncome)
+        {
+            var tax = 0;
+
+            return tax;
+        }
+    }
+}
--- PayrollService/Services/TaxCalculatorFactory.cs
+++ PayrollService/Services/TaxCalculatorFactory.cs
@@ -14,6 +14,7 @@ namespace PayrollService.Services
                 case "ESP":
                     return new SpainTaxDeductionCalculator();
                 case "DEU":
+                    return new GermanTaxDeductionCalculator();
                 default:
                     throw new NotImplementedException();
             }
