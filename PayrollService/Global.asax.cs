c9d4436872e5fe5b4f592de98886d08d55167fde
Tobias.Berger <Tobias.Berger@begis.de>
2020-02-26 20:30:56 +0100

    changed the json formatter to use lower case property names

--- PayrollService/Global.asax.cs
+++ PayrollService/Global.asax.cs
@@ -1,6 +1,9 @@
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Formatting;
 using System.Web;
 using System.Web.Http;
 using System.Web.Routing;
@@ -11,7 +14,16 @@ namespace PayrollService
     {
         protected void Application_Start()
         {
-            GlobalConfiguration.Configure(WebApiConfig.Register);
+            GlobalConfiguration.Configure(WebApiConfig.Register); 
+            GlobalConfiguration.Configuration.Formatters.Clear();
+            var jsonSerializerSettings = new JsonSerializerSettings
+            {
+                ContractResolver = new CamelCasePropertyNamesContractResolver()
+            };
+            GlobalConfiguration.Configuration.Formatters.Add(new JsonMediaTypeFormatter
+            {
+                SerializerSettings = jsonSerializerSettings
+            });
         }
     }
 }
